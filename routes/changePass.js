2d81436c7bb51a3470460e27bf1b070d2a7580d2
GhifariRizkiRamadhan <bmada2011@gmail.com>
2024-05-22 00:04:07 +0700

    done pt2

--- routes/changePass.js
+++ routes/changePass.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controller/ubahpassword')
+const middleware =require('../middleware/verifyToken')
 
 router.get('/ubahpassword', middleware.verifyToken, controller.ubahpassword)
 router.post('/ubahpassword', middleware.verifyToken, controller.formubahpassword)
